e910f141d7e00b4f8ab2efdc9c8a66d1b273fb54
Pawan201296 <pawansingh261094@gmail.com>
2023-11-23 15:50:51 +0530

    changes homepage

--- src/sections/home/Comit.js
+++ src/sections/home/Comit.js
@@ -27,7 +27,7 @@ export default function Comit() {
 							Our business
 						</div> */}
 						{/* <h4 className="pt_30 pb_20 text_lg color_white">Comit</h4> */}
-						<img src={ComitLogo.src} className={`${styles.ComitLogo} img-responsive pb_20`} />
+						<img src={ComitLogo.src} className={`${styles.ComitLogo} img-responsive pb_30`} />
 						<p className="color_white">
 						An app for everyone, no matter where they are in their journey to carbon neutrality. Comit brings carbon footprint offsetters to our platform, enabling them to trade multiple environmental products with ease in the most cost effective manner.
 						</p>
