7a50ac1cac062e3f1afd2e2747ab4a95f349c52a
Pawan201296 <pawansingh261094@gmail.com>
2023-11-23 16:26:47 +0530

    title and description added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 // MODULES //
-
+import { useEffect } from "react";
+import Script from "next/script";
 // COMPONENTS //
 
 // SECTIONS //
@@ -11,7 +12,24 @@ import "../src/styles/globals/globals.scss";
 
 /** App Page */
 function MyApp({ Component, pageProps }) {
-	return <Component {...pageProps} />;
+	return(
+    <>
+		 <Component {...pageProps} />
+		<script async src="https://www.googletagmanager.com/gtag/js?id=G-XQ1H1T1FWC"></script>
+    <Script>
+	{`
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-XQ1H1T1FWC');
+		`}
+    </Script>
+	</>
+	);
 }
 
 export default MyApp;
